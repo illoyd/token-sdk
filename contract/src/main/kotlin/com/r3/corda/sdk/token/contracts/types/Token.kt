29dbe7819881e5e316eb7f7f9bacb413b637b7a9
Ian Lloyd <ian.w.lloyd@gmail.com>
2019-02-17 16:22:34 +0800

    Thoughts on Token
    An option to obviate the need for EmbeddableToken.

--- contract/src/main/kotlin/com/r3/corda/sdk/token/contracts/types/Token.kt
+++ contract/src/main/kotlin/com/r3/corda/sdk/token/contracts/types/Token.kt
@@ -8,5 +8,9 @@ import net.corda.core.contracts.TokenizableAssetInfo
  * token to a particular owner. For that we use the [OwnedToken] state for assigning non-fungible tokens to an owner and
  * the [OwnedTokenAmount] state for assigning amounts of some fungible token to an owner. Currently, this interface is
  * just a marker and it will probably stay that way.
+ *
+ * TODO: Consider moving tokenIdentifier and tokenClass out of [AbstractOwnedToken] into the token definition.
+ * This way,  * the token is responsible for storing itself in the schema rather than needing to decide within the
+ * Contract. Also opens up the option of using any other thing as a token, and we can remove [EmbeddableToken].
  */
 interface Token : TokenizableAssetInfo
